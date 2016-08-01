8598111fb41b19cbc717622511d119eec9343f16
Juan Garcia <jgarcia@grubhub.com>
2016-08-01 18:05:51 -0500

    Agregando Gradle support

---
+++ AbstractFactory/ClienteAbstractFactory.java
@@ -0,0 +1,14 @@
+package com.ioet.training.abstractfactory
+
+class ClienteAbstractFactory {
+    public static void main(String[] args) {
+
+	DieteticoFactory platoDietetico = new DieteticoFactory();
+	ConYapaFactory platoConYapa = new ConYapaFactory();
+
+	platoDietetico.agregarArroz();
+	platoDietetico.agregar
+ 
+        // syso
+    }
+}
